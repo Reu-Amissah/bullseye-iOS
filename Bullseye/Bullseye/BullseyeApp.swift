57db884ecc1c7e54d9146762de0ef155863710fd
Reu-Amissah <reujoyamissah@gmail.com>
2022-09-27 23:24:56 +0000

    initial commit

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by R. Amissah on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
